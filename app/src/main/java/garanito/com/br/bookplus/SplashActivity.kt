b87c24d7fb41b61245c0b67220ef4d3393f860a1
Marcos Pereira Garanito <mgaranito@hotmail.com>
2019-04-15 21:08:51 -0300

    Correção de problemas, travando orientação, criando Splash Screen

---
+++ app/src/main/java/garanito/com/br/bookplus/SplashActivity.kt
@@ -0,0 +1,31 @@
+package garanito.com.br.bookplus
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_splash.*
+
+class SplashActivity : AppCompatActivity() {
+    private val TEMPO_AGUARDO_SPLASHSCREEN = 3500L
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        carregar()
+    }
+private fun carregar(){
+//Carrega a animacao
+    val anim = AnimationUtils.loadAnimation(this, R.anim.animacao_splash)
+    anim.reset()
+    ivLogo.clearAnimation()
+//Roda a animacao
+    ivLogo.startAnimation(anim)
+//Chama a próxima tela após 3,5 segundos definido na SPLASH_DISPLAY_LENGTH
+    Handler().postDelayed({
+        val proximaTela = Intent(this@SplashActivity, MainActivity::class.java)
+        startActivity(proximaTela)
+        finish()
+    }, TEMPO_AGUARDO_SPLASHSCREEN)
+}
+}
