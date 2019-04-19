4f6256c26e4ad4f9fe940d011ec540414036419b
Marcos Pereira Garanito <mgaranito@hotmail.com>
2019-04-19 11:08:23 -0300

    Implementando o firebase

--- app/src/main/java/garanito/com/br/bookplus/MainActivity.kt
+++ app/src/main/java/garanito/com/br/bookplus/MainActivity.kt
@@ -2,11 +2,33 @@ package garanito.com.br.bookplus
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.NonNull
+import android.text.TextUtils
+import android.widget.Button
+import android.widget.EditText
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+
+
+
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    private lateinit var mAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        FirebaseApp.initializeApp(this);
+        mAuth = FirebaseAuth.getInstance();
+
+        var currentUser = mAuth?.getCurrentUser()
     }
 }
+
