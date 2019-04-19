62837f120ea6afd1e5a54f17da252bdc5641ecc3
Marcos Pereira Garanito <mgaranito@hotmail.com>
2019-04-19 16:11:07 -0300

    Porrada de coisas arrumadas!!!

--- app/src/main/java/garanito/com/br/bookplus/MainActivity.kt
+++ app/src/main/java/garanito/com/br/bookplus/MainActivity.kt
@@ -1,34 +1,63 @@
 package garanito.com.br.bookplus
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.annotation.NonNull
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import com.google.android.gms.tasks.Task
 import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
-
-
-
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
 
     private lateinit var mAuth: FirebaseAuth
+    private val CADASTRO_REQUEST_CODE = 1
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //FirebaseApp.initializeApp(this.MainActivity)
         setContentView(R.layout.activity_main)
+        mAuth = FirebaseAuth.getInstance()
 
-        FirebaseApp.initializeApp(this);
-        mAuth = FirebaseAuth.getInstance();
+        if (mAuth.currentUser != null) {
+            vaiParaTelaMenu()
+        }
 
-        var currentUser = mAuth?.getCurrentUser()
+        btLogar.setOnClickListener {
+            mAuth.signInWithEmailAndPassword(
+                    etEmail.text.toString(),
+                    etSenha.text.toString()
+            ).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    vaiParaTelaMenu()
+                } else {
+                    Toast.makeText(this@MainActivity, it.exception?.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
+        btNovaConta.setOnClickListener {
+            val telaSeguinte = Intent(this, SignUpActivity::class.java)
+            startActivityForResult(telaSeguinte, CADASTRO_REQUEST_CODE)
+        }
+
+
     }
+
+    fun vaiParaTelaMenu(){
+        val telaSeguinteMenu = Intent(this, MenuActivity::class.java)
+        startActivityForResult(telaSeguinteMenu, CADASTRO_REQUEST_CODE)
+    }
+
 }
 
--- app/src/main/java/garanito/com/br/bookplus/SplashActivity.kt
+++ app/src/main/java/garanito/com/br/bookplus/SplashActivity.kt
@@ -7,25 +7,28 @@ import android.os.Handler
 import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_splash.*
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity()
+{
     private val TEMPO_AGUARDO_SPLASHSCREEN = 3500L
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
         carregar()
     }
-private fun carregar(){
-//Carrega a animacao
-    val anim = AnimationUtils.loadAnimation(this, R.anim.animacao_splash)
-    anim.reset()
-    ivLogo.clearAnimation()
-//Roda a animacao
-    ivLogo.startAnimation(anim)
-//Chama a próxima tela após 3,5 segundos definido na SPLASH_DISPLAY_LENGTH
-    Handler().postDelayed({
-        val proximaTela = Intent(this@SplashActivity, MainActivity::class.java)
-        startActivity(proximaTela)
-        finish()
-    }, TEMPO_AGUARDO_SPLASHSCREEN)
-}
+
+    private fun carregar() {
+
+        //Carrega a animacao
+        val anim = AnimationUtils.loadAnimation(this, R.anim.animacao_splash)
+        anim.reset()
+        ivLogo.clearAnimation()
+        //Roda a animacao
+        ivLogo.startAnimation(anim)
+        //Chama a próxima tela após 3,5 segundos definido na SPLASH_DISPLAY_LENGTH
+        Handler().postDelayed({
+            val proximaTela = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(proximaTela)
+            finish()
+        }, TEMPO_AGUARDO_SPLASHSCREEN)
+    }
 }
