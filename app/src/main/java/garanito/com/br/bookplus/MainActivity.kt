bb3516823aefdf4ecfc2db7620dd16cfee34e867
Marcos Pereira Garanito <mgaranito@hotmail.com>
2019-04-19 16:33:34 -0300

    Porrada de coisas arrumadas!!!

--- app/src/main/java/garanito/com/br/bookplus/MainActivity.kt
+++ app/src/main/java/garanito/com/br/bookplus/MainActivity.kt
@@ -3,16 +3,8 @@ package garanito.com.br.bookplus
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.annotation.NonNull
-import android.text.TextUtils
-import android.widget.Button
-import android.widget.EditText
 import android.widget.Toast
-import com.google.android.gms.tasks.Task
-import com.google.firebase.FirebaseApp
-import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
 
 
