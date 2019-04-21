6c81ce6959a8b2d39af7b01af47c5098197d116c
Marcos Pereira Garanito <mgaranito@hotmail.com>
2019-04-21 20:17:25 -0300

    Refactor Classes

--- app/src/main/java/garanito/com/br/bookplus/ui/SignUpActivity.kt
+++ app/src/main/java/garanito/com/br/bookplus/ui/SignUpActivity.kt
@@ -2,36 +2,79 @@ package garanito.com.br.bookplus.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import com.google.firebase.firestore.DocumentReference
-import com.google.firebase.firestore.FirebaseFirestore
 import garanito.com.br.bookplus.R
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 
 class SignUpActivity : AppCompatActivity() {
 
-    lateinit var db: DocumentReference
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        var documentPath = "documentos/cidades"
-        db = FirebaseFirestore.getInstance().document(documentPath)
+
 
         btCadastrar.setOnClickListener {
-            val city = HashMap<String, Any>()
-            city["name"] = "Los Angeles"
-            city["state"] = "CA"
-            city.put("country", "USA")
+            if (isValid(etNameSign.text.toString(), etEmail.text.toString(), etCep.text.toString().toDouble(), etConfirmPassword.text.toString(), etPasswordSign.text.toString())) {
 
-            db.collection("cidade").document("04")
-                    .set(city)
-                    .addOnSuccessListener { Log.d("SignUpActivity", "DocumentSnapshot successfully written!") }
-                    .addOnFailureListener { e -> Log.w("SignUpActivity", "Error writing document", e) }
+/*
+              var x = HashMap<String, Any>()
+              if(FirebaseRepository.InsertDocument(x,"documents","documents/usuers","users"))
+                  Toast.makeText(this,"Registro Inserido com Sucesso!",Toast.LENGTH_LONG)
+  */
+            }
         }
+
 
     }
 
+    fun isValid(name: String, email: String, zipocode: Double, repeatpassword: String, password: String): Boolean {
+
+        val str: String = getString(R.string.lblObrigatorio)
+        val strPasswd: String = getString(R.string.passwordNotMatch)
+        if (zipocode < 1) {
+            etCep.error = str
+            etCep.requestFocus()
+            return false
+        }
+
+        if (name.isEmpty()) {
+            etNameSign.error = str
+            etNameSign.requestFocus()
+            return false
+        }
+        if (email.isEmpty()) {
+            etEmailSign.error = str
+            etEmailSign.requestFocus()
+            return false
+        }
+
+        if (password.isEmpty()) {
+            etPasswordSign.error = str
+            etPasswordSign.requestFocus()
+            return false
+        }
+        if (repeatpassword.isEmpty()) {
+            etConfirmPassword.error = str
+            etConfirmPassword.requestFocus()
+            return false
+        }
+        if (password != repeatpassword) {
+            etPasswordSign.setText("")
+            etConfirmPassword.setText("")
+            etPasswordSign.error = strPasswd
+            etPasswordSign.requestFocus()
+            return false
+        }
+
+        if (password.isEmpty()) {
+            etEmailSign.error = str
+            etEmailSign.requestFocus()
+            return false
+        }
+        return true
+    }
 
 }
