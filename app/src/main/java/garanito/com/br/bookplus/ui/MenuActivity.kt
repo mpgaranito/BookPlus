3c49cdadfe542a6bba7ed6f81e261a02dd0cb247
Marcos Pereira Garanito <mgaranito@hotmail.com>
2019-04-20 23:47:31 -0300

    Tela cadastro usuario

--- app/src/main/java/garanito/com/br/bookplus/ui/MenuActivity.kt
+++ app/src/main/java/garanito/com/br/bookplus/ui/MenuActivity.kt
@@ -14,22 +14,24 @@ import kotlinx.android.synthetic.main.activity_menu.*
 class MenuActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener,
         BottomNavigationView.OnNavigationItemReselectedListener{
     override fun onNavigationItemSelected(p0: MenuItem): Boolean {
-
+        when (p0.itemId){
+           R.id.nav_exit -> logoff()
+        }
         Log.i("oka","ola")
-
         return true
     }
 
     override fun onNavigationItemReselected(p0: MenuItem) {
       when (p0.itemId){
-          0 -> logoff()
+         R.id.nav_exit -> logoff()
       }
-        Log.i("oka","ola")
         return
     }
 
     private fun logoff() {
-        FirebaseAuth.getInstance().signOut();
+        FirebaseAuth.getInstance().signOut()
+        finishAffinity()
+        Log.i("logOff","Saindo da Aplicacao")
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
