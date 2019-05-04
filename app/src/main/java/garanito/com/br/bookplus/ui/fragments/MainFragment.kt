942ad2be8d0d517270170887718e31532bed6e5d
Marcos Pereira Garanito <mgaranito@hotmail.com>
2019-05-04 12:24:02 -0300

    Dialog Recycler

--- app/src/main/java/garanito/com/br/bookplus/ui/fragments/MainFragment.kt
+++ app/src/main/java/garanito/com/br/bookplus/ui/fragments/MainFragment.kt
@@ -1,6 +1,7 @@
 package garanito.com.br.bookplus.ui.fragments
 
 
+import android.app.AlertDialog
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -10,6 +11,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import garanito.com.br.bookplus.R
 import garanito.com.br.bookplus.adapter.FairAdapter
 import garanito.com.br.bookplus.manager.BookFairManager
@@ -28,6 +30,17 @@ private const val ARG_PARAM2 = "param2"
 class MainFragment : Fragment() {
     lateinit var myDb: BookFairManager
 
+    fun realtime(rvFairs: RecyclerView): FairAdapter {
+        myDb = BookFairManager(activity)
+        var xptoFair = myDb.select()
+
+        val adapterFair = FairAdapter(xptoFair, activity, {})
+        rvFairs.adapter = adapterFair
+        val layoutManager = LinearLayoutManager(activity)
+        rvFairs.layoutManager = layoutManager
+        return adapterFair
+    }
+
     companion object {
         fun newInstance(): MainFragment {
             return MainFragment()
@@ -37,16 +50,10 @@ class MainFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_main, container, false)
-        myDb = BookFairManager(activity)
-        var xptoFair = myDb.select()
-        val rvFairs = view.findViewById(garanito.com.br.bookplus.R.id.rvFairs) as RecyclerView
-        val adapterFair = FairAdapter(xptoFair, activity, {
-            Log.i("holf", "eeee")
-        })
-        rvFairs.adapter = adapterFair
-        val layoutManager = LinearLayoutManager(activity)
-        rvFairs.layoutManager = layoutManager
-        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+        val rvFairs = view!!.findViewById(garanito.com.br.bookplus.R.id.rvFairs) as RecyclerView
+        var adapterFair = realtime(rvFairs)
+
+        val itemTouchHelperCallback = object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
             override fun onMove(p0: RecyclerView, p1: RecyclerView.ViewHolder, p2: RecyclerView.ViewHolder): Boolean {
                 return false
             }
@@ -54,11 +61,26 @@ class MainFragment : Fragment() {
             override fun onSwiped(p0: RecyclerView.ViewHolder, p1: Int) {
                 Log.i("swipe", "slipe" + p1)
                 try {
-                    adapterFair.removeItem(p0, myDb)
+                    val builder = AlertDialog.Builder(activity)
+                    val strDelete: String = getString(R.string.Removed)
+                    builder.setTitle("Ops")
+                    builder.setMessage(strDelete)
+                    builder.setPositiveButton(getString(R.string.YES)) { dialog, which ->
+                        adapterFair.removeItem(p0, myDb)
+                        Toast.makeText(activity, strDelete, Toast.LENGTH_SHORT).show()
+                    }
+                    builder.setNegativeButton(getString(R.string.NO)) { dialog, which ->
+                        adapterFair = realtime(rvFairs)
+
+                    }
+                    builder.setNeutralButton(getString(R.string.CANCEL)) { _, _ ->
+                        adapterFair = realtime(rvFairs)
+                    }
+                    val dialog: AlertDialog = builder.create()
+                    dialog.show()
                 } catch (ex: Exception) {
                     Log.e("swipe", ex.toString())
                 }
-
             }
 
         }
